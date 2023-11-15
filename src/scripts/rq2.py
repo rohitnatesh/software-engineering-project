e47c07be9f299bb0edd94afeb33a8e74f296d768
Rohit Natesh <rohit.natesh95@gmail.com>
2023-11-15 16:33:08 -0500

    Plot the graph for rq2

--- src/scripts/rq2.py
+++ src/scripts/rq2.py
@@ -7,6 +7,7 @@ import os
 import json
 import pandas as pd
 from glob import glob 
+import matplotlib.pyplot as plt
 
 CURRENT_DIRECTORY = os.getcwd()
 RAW_DATASETS_FILE_PATTERN = "/src/raw_datasets/*/*_"
@@ -40,23 +41,70 @@ def process_data(category_dfs):
 def write_processed_df(df, file):
     df.to_json(f"{CURRENT_DIRECTORY}{OUTPUT_FOLDER_PATH}{file.lower().replace(' ', '_')}.json", orient="records")
     df.to_csv(f"{CURRENT_DIRECTORY}{OUTPUT_FOLDER_PATH}{file.lower().replace(' ', '_')}.csv", index=False)
-
+    print(f"\tProcessed data exported.")
 
 def prepare_datasets():
+    datasets = {}
     for file in DATASET_CATEGORIES:
         print(f"\nPreparing category: {file}")
         filepaths = glob(f"{CURRENT_DIRECTORY}{RAW_DATASETS_FILE_PATTERN}{file.lower().replace(' ','_')}.json")
         category_dfs = [read_datasets(filepath) for filepath in filepaths]            
         processed_df = process_data(category_dfs)
-        write_processed_df(processed_df, file)
-        print(f"\tProcessed data exported.")
+        
+        # write_processed_df(processed_df, file)
+        datasets[file] = processed_df
 
         del category_dfs
         del processed_df
 
+    return datasets
+
+
+def plot_graphs(datasets):
+    plt.rcParams.update({'figure.autolayout': True})
+
+    for df_key in datasets:
+        df = datasets[df_key]
+        df['DateOfConversation'] = pd.to_datetime(df['DateOfConversation'])
+        df['Week'] = df['DateOfConversation'].dt.to_period('W')
+ 
+        unique_weeks = sorted(df['Week'].unique())
+        top_languages_count = 5
+        alpha = 0.1
+        stacked_data = []
+        top_languages_all_weeks = set()
+        
+        for week in unique_weeks:
+            week_data = df[df['Week'] == week]        
+            top_languages = week_data['RepoLanguage'].value_counts().nlargest(top_languages_count).index
+            top_languages_all_weeks.update(top_languages)
+
+
+        for week in unique_weeks:        
+            week_data = df[df['Week'] == week]
+            top_languages = week_data['RepoLanguage'].value_counts().nlargest(top_languages_count).index
+            language_counts = week_data['RepoLanguage'].value_counts().reindex(top_languages_all_weeks, fill_value=0)
+            other_languages = week_data[~week_data['RepoLanguage'].isin(top_languages_all_weeks)]['RepoLanguage'].unique()
+            language_counts['Others'] = week_data[week_data['RepoLanguage'].isin(other_languages)]['RepoLanguage'].count()
+            
+            language_counts_smoothed = language_counts.ewm(alpha=alpha).mean()
+
+            stacked_data.append(language_counts_smoothed)
+
+        stacked_df = pd.concat(stacked_data, axis=1, keys=unique_weeks)
+
+        stacked_df.plot(kind='bar', colormap='viridis')
+        plt.title(f'{df_key}: Graph of Top {top_languages_count} RepoLanguages')
+        plt.xlabel('Programming Language')
+        plt.ylabel('Count')
+        plt.legend(title='Time', bbox_to_anchor=(1, 1), loc='upper left')
+
+    plt.show()
+
 
 def main():
-    prepare_datasets()
+    datasets = prepare_datasets()
+    plot_graphs(datasets)
 
 
 main()
