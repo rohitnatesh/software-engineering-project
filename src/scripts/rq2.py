93476a7f0b51422178ae5883ff55e5d76e7e869c
Rohit Natesh <rohit.natesh95@gmail.com>
2023-12-06 13:33:32 -0500

    Analyze data and plot

--- src/scripts/rq2.py
+++ src/scripts/rq2.py
@@ -6,8 +6,10 @@
 import os
 import json
 import pandas as pd
-from glob import glob 
+from glob import glob
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
 CURRENT_DIRECTORY = os.getcwd()
 RAW_DATASETS_FILE_PATTERN = "/src/raw_datasets/*/*_"
@@ -17,7 +19,7 @@ DATASET_CATEGORIES = [
     "Issue Sharings",
     "Discussion Sharings",
 ]
-OUTPUT_FOLDER_PATH = "/src/filtered_datasets/rq2/"
+FILTERED_DATA_FOLDER_PATH = "/src/filtered_datasets/rq2/"
 
 
 def read_datasets(filepath):
@@ -29,82 +31,106 @@ def read_datasets(filepath):
 
 
 def process_data(category_dfs):
-    print(f"\n\tProcessing category data...")
+    print("\n\tProcessing category data...")
     df = pd.concat(category_dfs)
     chatgpt_sharing = pd.json_normalize(df["ChatgptSharing"].map(lambda x: x[0]))
-    conversation_date = pd.to_datetime(chatgpt_sharing["DateOfConversation"], errors="coerce").dt.strftime("%m-%d-%Y")
+    conversation_date = pd.to_datetime(
+        chatgpt_sharing["DateOfConversation"], errors="coerce"
+    ).dt.strftime("%m-%d-%Y")
     processed_df = df.drop("ChatgptSharing", axis=1).join(conversation_date)
 
-    return processed_df[(processed_df.RepoLanguage.notnull()) & (processed_df.DateOfConversation.notnull())]
+    return processed_df[
+        (processed_df.RepoLanguage.notnull())
+        & (processed_df.DateOfConversation.notnull())
+    ]
 
-
-def write_processed_df(df, file):
-    df.to_json(f"{CURRENT_DIRECTORY}{OUTPUT_FOLDER_PATH}{file.lower().replace(' ', '_')}.json", orient="records")
-    df.to_csv(f"{CURRENT_DIRECTORY}{OUTPUT_FOLDER_PATH}{file.lower().replace(' ', '_')}.csv", index=False)
-    print(f"\tProcessed data exported.")
 
 def prepare_datasets():
-    datasets = {}
     for file in DATASET_CATEGORIES:
         print(f"\nPreparing category: {file}")
-        filepaths = glob(f"{CURRENT_DIRECTORY}{RAW_DATASETS_FILE_PATTERN}{file.lower().replace(' ','_')}.json")
-        category_dfs = [read_datasets(filepath) for filepath in filepaths]            
+        filepaths = glob(
+            f"{CURRENT_DIRECTORY}{RAW_DATASETS_FILE_PATTERN}{file.lower().replace(' ','_')}.json"
+        )
+        category_dfs = [read_datasets(filepath) for filepath in filepaths]
         processed_df = process_data(category_dfs)
-        
-        # write_processed_df(processed_df, file)
-        datasets[file] = processed_df
+
+        processed_df.to_csv(
+            f"{CURRENT_DIRECTORY}{FILTERED_DATA_FOLDER_PATH}{file.lower().replace(' ', '_')}.csv",
+            index=False,
+        )
 
         del category_dfs
         del processed_df
+    print("\tProcessed data exported.")
 
-    return datasets
 
+def analyze_data():
+    datasets = {}
 
-def plot_graphs(datasets):
-    plt.rcParams.update({'figure.autolayout': True})
+    for category in DATASET_CATEGORIES:
+        file_path = f"{CURRENT_DIRECTORY}{FILTERED_DATA_FOLDER_PATH}{category.lower().replace(' ', '_')}.csv"
+        df = pd.read_csv(file_path, header=0)
+        datasets[category] = df
+
+    plt.rcParams.update({"figure.autolayout": True})
 
     for df_key in datasets:
+        plt.figure(figsize=(10, 6))
         df = datasets[df_key]
-        df['DateOfConversation'] = pd.to_datetime(df['DateOfConversation'])
-        df['Week'] = df['DateOfConversation'].dt.to_period('W')
- 
-        unique_weeks = sorted(df['Week'].unique())
-        top_languages_count = 5
-        alpha = 0.1
-        stacked_data = []
-        top_languages_all_weeks = set()
-        
-        for week in unique_weeks:
-            week_data = df[df['Week'] == week]        
-            top_languages = week_data['RepoLanguage'].value_counts().nlargest(top_languages_count).index
-            top_languages_all_weeks.update(top_languages)
+        df["DateOfConversation"] = pd.to_datetime(df["DateOfConversation"])
 
+        top_languages = df["RepoLanguage"].value_counts().nlargest(5).index
 
-        for week in unique_weeks:        
-            week_data = df[df['Week'] == week]
-            top_languages = week_data['RepoLanguage'].value_counts().nlargest(top_languages_count).index
-            language_counts = week_data['RepoLanguage'].value_counts().reindex(top_languages_all_weeks, fill_value=0)
-            other_languages = week_data[~week_data['RepoLanguage'].isin(top_languages_all_weeks)]['RepoLanguage'].unique()
-            language_counts['Others'] = week_data[week_data['RepoLanguage'].isin(other_languages)]['RepoLanguage'].count()
-            
-            language_counts_smoothed = language_counts.ewm(alpha=alpha).mean()
+        plt.title(f"Top 5 Programming Languages Over Time: {df_key}")
+        plt.xlabel("DateOfConversation")
+        plt.ylabel("Count")
 
-            stacked_data.append(language_counts_smoothed)
+        for language in top_languages:
+            language_data = df[df["RepoLanguage"] == language]
+            language_counts = (
+                language_data.groupby(pd.Grouper(key="DateOfConversation", freq="W"))
+                .size()
+                .reset_index(name="Count")
+            )
 
-        stacked_df = pd.concat(stacked_data, axis=1, keys=unique_weeks)
+            sns.scatterplot(
+                x=language_counts["DateOfConversation"],
+                y=language_counts["Count"],
+                label=language,
+                s=15,
+            )
 
-        stacked_df.plot(kind='bar', colormap='viridis')
-        plt.title(f'{df_key}: Graph of Top {top_languages_count} RepoLanguages')
-        plt.xlabel('Programming Language')
-        plt.ylabel('Count')
-        plt.legend(title='Time', bbox_to_anchor=(1, 1), loc='upper left')
+            window_size = 5
+            rolling_average = (
+                language_counts["Count"]
+                .rolling(window=window_size, min_periods=1)
+                .mean()
+            )
+
+            x = np.arange(len(language_counts["DateOfConversation"]))
+            coeffs = np.polyfit(x, rolling_average, 1)
+            trend = np.poly1d(coeffs)
+            plt.plot(language_counts["DateOfConversation"], trend(x), linestyle="--")
+
+        plt.legend(loc="upper left", bbox_to_anchor=(1, 1))
+        plt.xticks(rotation=45)
+        plt.tight_layout()
 
     plt.show()
 
 
 def main():
-    datasets = prepare_datasets()
-    plot_graphs(datasets)
+    while True:
+        choice = input(
+            "\n1. Data Processing\n2. Data Analysis\n3. Exit\nEnter choice: "
+        )
+
+        if choice == "1":
+            prepare_datasets()
+        elif choice == "2":
+            analyze_data()
+        else:
+            break
 
 
 main()
